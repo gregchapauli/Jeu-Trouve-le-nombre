d5bdefcbd779b7726df045990ab0048818495e3b
Gregory Caloin <gregchapauli@gmail.com>
2020-06-14 10:22:59 +0200

    Mise en place des "const" dans "app.js"

---
+++ app.js
@@ -0,0 +1,22 @@
+// Elements du DOM
+const divVies = document.querySelector('.vies');
+const message = document.getElementById('message');
+const formulaire = document.getElementById('inputBox');
+const input = document.getElementById('number');
+const essayerBtn = document.getElementById('essayerBtn');
+const rejouerBtn = document.getElementById('rejouer');
+const body = document.getElementsByTagName('body')[0];
+
+//Modèle de coeurs
+const coeurVide = '<ion-icon name="heart-outline"></ion-icon>';
+const coeurPlein = '<ion-icon name="heart"></ion-icon>';
+
+// Fond
+const bgFroid = 'linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%)';
+const bgTiede = 'linear-gradient(120deg, #f6d365 0%, #fda085 100%)';
+const bgChaud = 'linear-gradient(to right, #f83600 0%, #f9d423 100%)';
+const bgBrulant = 'linear-gradient(to right, #ff8177 0%, #ff867a 0%, #ff8c7f 21%, #f99185 52%, #cf556c 78%, #b12a5b 100%)';
+
+const bgWin = 'linear-gradient(to right, #4facfe 0%, #00f2fe 100%)';
+const bgLoose = 'background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898';
+
