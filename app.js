f10119bd9a1524e34e62f26ddcac55365c645c79
Gregory Caloin <gregchapauli@gmail.com>
2020-06-14 10:37:51 +0200

    Mise en place de la logique : "nombre aléatoire" et cas gagnant

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const essayerBtn = document.getElementById('essayerBtn');
 const rejouerBtn = document.getElementById('rejouer');
 const body = document.getElementsByTagName('body')[0];
 
-//Modèle de coeurs
+// Modèle de coeurs
 const coeurVide = '<ion-icon name="heart-outline"></ion-icon>';
 const coeurPlein = '<ion-icon name="heart"></ion-icon>';
 
@@ -20,3 +20,32 @@ const bgBrulant = 'linear-gradient(to right, #ff8177 0%, #ff867a 0%, #ff8c7f 21%
 const bgWin = 'linear-gradient(to right, #4facfe 0%, #00f2fe 100%)';
 const bgLoose = 'background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898';
 
+// PLAY : 
+const play = () => {
+
+    // nombre aléatoire
+    const randomNumber = Math.floor(Math.random() * 101);
+    const totalVies = 6;
+    let vies = totalVies;
+    console.log(randomNumber);
+
+    // actualisation à chaque essai - TOUTE LA LOGIQUE
+    formulaire.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const valeurtInput = parseInt(input.value);
+
+        if(valeurInput < 0 || valeurInput > 100) return;
+        
+        if(valeurInput === randomNumber){
+            body.style.backgroundImage = bgWin;
+            message.textContent = `BRAVO !!! Le nombre était bien ${randomNumber}`;
+            rejouerBtn.style.display = "block";
+        }
+
+        if(valeurInput !== randomNumber) {
+
+        }
+        
+    })
+
+}
